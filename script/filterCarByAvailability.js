1d2dd8e1b5dda1553aaf19fac35bf30a625b1447
alreagon <alpharajasa@gmail.com>
2024-04-19 19:41:55 +0700

    add filterCarByAvailability.js

--- script/filterCarByAvailability.js
+++ script/filterCarByAvailability.js
@@ -1,12 +1,22 @@
 function filterCarByAvailability(cars) {
-  // Sangat dianjurkan untuk console.log semua hal hehe
   console.log(cars);
 
-  // Tempat penampungan hasil
   const result = [];
 
-  // Tulis code-mu disini
+  for (let i = 0; i < cars.length; i++) {
+    if (cars[i].available) {
+      result.push(cars[i]);
+    }
+  }
 
-  // Rubah code ini dengan array hasil filter berdasarkan availablity
+  // for (const car of cars) {
+  //   if (car.available) {
+  //     result.push(car);
+  //   }
+  // }
+
+  console.log(result);
+  console.table(result);
+
   return result;
 }
